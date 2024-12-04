7eb626c3ce07a3c996a8a84ccf569705cbae000d
Rhara Ianna <rharaianna@gmail.com>
2024-12-04 17:08:13 -0300

    Adicionando configurações de quebras de linha no .gitattributes

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,56 @@
+public class ContaBancaria {
+
+    private String titular;
+    private String numero_conta;
+    private double saldo;
+
+    public ContaBancaria () {
+        this.saldo = 0;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public String getNumero_conta() {
+        return numero_conta;
+    }
+    public void setNumero_conta(String numero_conta) {
+        if (numero_conta.length() != 8) {
+            throw new IllegalArgumentException("O número da conta deve ter exatamente 8 caracteres");
+        }
+        else {
+            this.numero_conta = numero_conta;
+        }
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    private void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public void depositar (double valor) {
+        if (valor < 0){
+            throw new IllegalArgumentException("Valor de depósito não pode ser negativo");
+        }
+        else {
+            this.saldo += valor;
+        }
+    }
+    public void sacar (double valor) {
+        if (valor >= 0){
+            if (this.saldo >= valor) {
+                this.saldo -= valor;
+            }
+            else {
+                throw new IllegalArgumentException("Saldo insuficiente");
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Valor de saque não pode ser negativo");
+        }
+    }
+}
---
+++ src/test/java/ContaBancariaTest.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ContaBancariaTest {
+
+    @Test
+    void tiraMaisDoQueTem() {
+        try {
+            ContaBancaria conta = new ContaBancaria();
+            conta.depositar(99);
+            conta.sacar(100);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Saldo insuficiente", e.getMessage(),
+                    "A mensagem da exceção está incorreta.");
+        }
+    }
+
+    @Test
+    void saqueMenorQueZero() {
+        try {
+            ContaBancaria conta = new ContaBancaria();
+            conta.sacar(-1);
+            fail();
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("Valor de saque não pode ser negativo", e.getMessage());
+        }
+    }
+
+    @Test
+    void depositoMenorQueZero() {
+        try {
+            ContaBancaria conta = new ContaBancaria();
+            conta.depositar(-1);
+            fail();
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("Valor de depósito não pode ser negativo", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveSomarDeposito(){
+        ContaBancaria conta = new ContaBancaria();
+        conta.depositar(100);
+        conta.depositar(90);
+        assertEquals(190, conta.getSaldo());
+    }
+
+    @Test
+    void deveRetirarSaque(){
+        ContaBancaria conta = new ContaBancaria();
+        conta.depositar(100);
+        conta.sacar(90);
+        assertEquals(10, conta.getSaldo());
+    }
+
+    @Test
+    void deveTerOitoDigitos(){
+        ContaBancaria conta = new ContaBancaria();
+        conta.setNumero_conta("12345678");
+        assertEquals("12345678", conta.getNumero_conta());
+    }
+
+    @Test
+    void tentaNoveDigitos(){
+        try{
+            ContaBancaria conta = new ContaBancaria();
+            conta.setNumero_conta("123456789");
+            fail();
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("O número da conta deve ter exatamente 8 caracteres", e.getMessage());
+        }
+    }
+
+    @Test
+    void tentaSeteDigitos() {
+        try{
+            ContaBancaria conta = new ContaBancaria();
+            conta.setNumero_conta("1234567");
+            fail();
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("O número da conta deve ter exatamente 8 caracteres", e.getMessage());
+        }
+    }
+}
+
